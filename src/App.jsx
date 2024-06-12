3c5bb88df1c519eba6c39176d29af893c6066fbd
Mikey Nichols <mnix@journeytocode.io>
2024-06-12 00:39:45 -0400

    cleans up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { useState, useMemo, useEffect, useRef } from "react";
-import extractJSON from "./utils/extractJson";
-import ColorPicker from "./components/ColorPicker";
-import InputForm from "./components/InputForm";
 import Header from "./components/Header";
 import MainSection from "./components/MainSection";
-import ResultsSection from "./components/ResultsSection";
 import InputSection from "./components/InputSection";
 import Footer from "./Footer";
 
