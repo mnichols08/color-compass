a03bedfd91b2e0bd33e333ee8c35d2861ee9064d
Mikey Nichols <mnix@journeytocode.io>
2024-06-12 00:38:38 -0400

    moves the state and logic for the input field into the InputSection component

--- src/App.jsx
+++ src/App.jsx
@@ -7,78 +7,13 @@ import MainSection from "./components/MainSection";
 import ResultsSection from "./components/ResultsSection";
 import InputSection from "./components/InputSection";
 import Footer from "./Footer";
-import askGroq from "./utils/groq";
 
 function App() {
-  const [hexColor, setHexColor] = useState("#f00");
-  const [usageEmpty, setUsageEmpty] = useState(false);
-  const [trigger, setTrigger] = useState(false);
-  const [buttonClicked, setButtonClicked] = useState(false);
-  const formDataRef = useRef({
-    usage: "Coordinate colors for my outfit",
-    colorScheme: "complimentary",
-  });
-  const colorArrRef = useRef([]);
-  const promptRef = useRef(formDataRef.current.usage);
-
-  useEffect(() => {
-    promptRef.current = formDataRef.current.usage;
-  }, [trigger]);
-  useEffect(() => {
-    promptRef.current = formDataRef.current.usage;
-  }, [formDataRef.current.usage]);
-  const colors = useMemo(() => colorArrRef.current, [colorArrRef.current]);
-  const prompt = useMemo(() => promptRef.current, [promptRef.current]);
-
-  const handleClick = async () => {
-    if (formDataRef.current.usage === "") {
-      setUsageEmpty(true);
-      return;
-    }
-    try {
-      const schemeObj = await askGroq(hexColor, formDataRef);
-      colorArrRef.current = schemeObj;
-      setButtonClicked(true);
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
-
-  const setters = useMemo(
-    () => ({
-      onChangeColor(color) {
-        setHexColor(color.hexString);
-      },
-    }),
-    []
-  );
-  const handleChange = (e) => {
-    e.preventDefault?.();
-    const changedField = e.target.name;
-    const newValue = e.target.value;
-    const breakingChars = /[^a-zA-Z0-9 ]/g;
-    if (!breakingChars.test(newValue)) {
-      formDataRef.current = {
-        ...formDataRef.current,
-        [changedField]: newValue,
-      };
-      setTrigger((prev) => prev + 1);
-      setUsageEmpty(false);
-    }
-  };
   return (
     <>
       <Header />
       <MainSection />
-      <InputSection
-        setters={setters}
-        handleChange={handleChange}
-        handleClick={handleClick}
-        formData={formDataRef.current}
-        hexColor={hexColor}
-        usageEmpty={usageEmpty}
-      />
-      <ResultsSection colors={colors} prompt={prompt} />
+      <InputSection />
       <Footer />
     </>
   );
--- src/components/InputSection.jsx
+++ src/components/InputSection.jsx
@@ -1,7 +1,10 @@
+import { useEffect, useMemo, useRef, useState } from "react";
+import askGroq from "../utils/groq";
 import Wrapper from "./Wrapper";
-import ColorPicker from "../components/ColorPicker";
-import InputForm from "../components/InputForm";
-import TextArea from "../components/TextArea";
+import ColorPicker from "./ColorPicker";
+import InputForm from "./InputForm";
+import TextArea from "./TextArea";
+import ResultsSection from "./ResultsSection";
 
 const options = {
   width: 422,
@@ -10,16 +13,63 @@ const options = {
   borderColor: "#ffffff",
 }; //options for color picker
 
-function InputSection({
-  setters,
-  handleChange,
-  formData,
-  hexColor,
-  handleClick,
-  usageEmpty,
-}) {
+function InputSection({}) {
+  const [hexColor, setHexColor] = useState("#f00");
+  const [usageEmpty, setUsageEmpty] = useState(false);
+  const [trigger, setTrigger] = useState(false);
+  const formDataRef = useRef({
+    usage: "Coordinate colors for my outfit",
+    colorScheme: "complimentary",
+  });
+  const colorArrRef = useRef([]);
+  const promptRef = useRef(formDataRef.current.usage);
+
+  useEffect(() => {
+    promptRef.current = formDataRef.current.usage;
+  }, [trigger]);
+  useEffect(() => {
+    promptRef.current = formDataRef.current.usage;
+  }, [formDataRef.current.usage]);
+  const colors = useMemo(() => colorArrRef.current, [colorArrRef.current]);
+  const prompt = useMemo(() => promptRef.current, [promptRef.current]);
+
+  const handleClick = async () => {
+    if (formDataRef.current.usage === "") {
+      setUsageEmpty(true);
+      return;
+    }
+    try {
+      const schemeObj = await askGroq(hexColor, formDataRef);
+      colorArrRef.current = schemeObj;
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const setters = useMemo(
+    () => ({
+      onChangeColor(color) {
+        setHexColor(color.hexString);
+      },
+    }),
+    []
+  );
+  const handleChange = (e) => {
+    e.preventDefault?.();
+    const changedField = e.target.name;
+    const newValue = e.target.value;
+    const breakingChars = /[^a-zA-Z0-9 ]/g;
+    if (!breakingChars.test(newValue)) {
+      formDataRef.current = {
+        ...formDataRef.current,
+        [changedField]: newValue,
+      };
+      setTrigger((prev) => prev + 1);
+      setUsageEmpty(false);
+    }
+  };
   return (
-    <main>
+    <>
       <section className="bg-[url('/src/img/input-section-bg.png')] bg-cover bg-center w-full h-screen border-y border-secondary-color">
         <Wrapper>
           <div className="md:grow text-sm md:text-base block lg:flex w-full items-center space-y-1 text-center text-primary-dark mt-80">
@@ -27,7 +77,7 @@ function InputSection({
               <ColorPicker options={options} setters={setters} />
               <InputForm
                 handleChange={handleChange}
-                formData={formData}
+                formData={formDataRef.current}
                 hexColor={hexColor}
                 usageEmpty={usageEmpty}
               />
@@ -36,14 +86,15 @@ function InputSection({
             <div className="md:grow text-sm md:text-base flex flex-col items-center space-y-1 text-center text-primary-dark lg:w-1/2 block w-full h-full">
               <TextArea
                 handleChange={handleChange}
-                formData={formData}
+                formData={formDataRef.current}
                 handleClick={handleClick}
               />
             </div>
           </div>
         </Wrapper>
       </section>
-    </main>
+      <ResultsSection colors={colors} prompt={prompt} />
+    </>
   );
 }
 
