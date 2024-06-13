6c832be435426917446bd1f3bcd3844ccf30d16c
Mikey Nichols <mnix@journeytocode.io>
2024-06-13 01:51:17 -0400

    updates sizing and utility of color input/outputs

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,51 +1,45 @@
 import "./InputForm.css";
 import Wrapper from "./Wrapper";
+import { setContrastText } from "../utils/setContrastText";
 
 export default function InputForm({ hexColor }) {
-  return (
+   return (
     <Wrapper>
       <div className="scheme-container bg-primary-color space-y-2.5 my-3 py-2.5 ">
         <div className="flex justify-center mx-2.5 gap-x-0.5 scheme-buttons-container">
-          <div>
-            <button
-              style={{ width: "130px" }}
-              className="rounded-full border-white border-2 scheme-button"
-            >
-              Analogue
-            </button>
-          </div>
-          <div>
-            <button
-              style={{ width: "130px" }}
-              className="rounded-full border-white border-2 scheme-button"
-            >
-              Triadic
-            </button>
-          </div>
-          <div>
-            <button
-              style={{ width: "150px" }}
-              className="rounded-full border-white border-2 scheme-button"
-            >
-              Complimentary
-            </button>
+          <div className="flex justify-center mx-2.5 gap-x-0.5 scheme-buttons-container">
+            <div>
+              <button className="rounded-full border-white border-2 scheme-button bg-brand-red text-white w-32">
+                Analogue
+              </button>
+            </div>
+            <div>
+              <button className="rounded-full border-white border-2 scheme-button bg-brand-light-green text-white w-32">
+                Triadic
+              </button>
+            </div>
+            <div>
+              <button className="rounded-full border-white border-2 scheme-button bg-primary-dark text-white w-36">
+                Complimentary
+              </button>
+            </div>
           </div>
         </div>
         <div className="shadow-pink-50 flex justify-center mx-2.5 scheme-hexCodes-container">
           <div
-            style={{ background: "#31D7BE", borderRadius: "40px 0 0 40px" }}
+            style={{ background: hexColor, borderRadius: "40px 0 0 40px", color: setContrastText(hexColor) }}
             className="flex justify-center items-center border-white border-2 hex-codes"
           >
             {hexColor}
           </div>
           <div
-            style={{ background: "#BE31D7" }}
+            style={{ background: hexColor, color: setContrastText(hexColor) }}
             className="flex justify-center items-center border-white border-2  hex-codes"
           >
             {hexColor}
           </div>
           <div
-            style={{ background: "#D7BE31", borderRadius: " 0 40px 40px 0" }}
+            style={{ background: hexColor, borderRadius: " 0 40px 40px 0", color: setContrastText(hexColor)}}
             className="flex justify-center items-center border-white border-2 hex-codes"
           >
             {hexColor}
